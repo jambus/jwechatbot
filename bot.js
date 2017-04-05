dec582abcfd9a15eaf887f24a51160b2f666921d
Liang Shen <Liang.Shen@blackboard.com>
2017-04-05 22:37:42 +0800

    Linked with tuling

--- bot.js
+++ bot.js
@@ -1,59 +1,74 @@
-import {Wechaty, Room} from 'wechaty'
+const { Wechaty } = require('wechaty');
+const http = require('http');
+const querystring = require('querystring');  
 
-const bot = Wechaty.instance()
+const tulingAPIKey = 'e341f3cbed2f4faa9604df3f10246661';
 
-bot
-.on('scan', (url, code)=>{
-    let loginUrl = url.replace('qrcode', 'l')
-    require('qrcode-terminal').generate(loginUrl)
-    console.log(url)
-})
+Wechaty.instance() // Singleton
+    .on('scan', (url, code) => console.log(`Scan QR Code to login: ${code}\n${url}`))
 
-.on('login', user=>{
-    console.log(`${user} login`)
-})
+.on('login', user => console.log(`User ${user} logined`))
 
-.on('friend', async function (contact, request){
-    if(request){
-        await request.accept()
-        console.log(`Contact: ${contact.name()} send request ${request.hello}`)
-    }
-})
+.on('message', message => callTuling(message))
 
-.on('message', async function(m){
-    const contact = m.from()
-    const content = m.content()
-    const room = m.room()
+.init();
 
-    if(room){
-        console.log(`Room: ${room.topic()} Contact: ${contact.name()} Content: ${content}`)
-    } else{
-        console.log(`Contact: ${contact.name()} Content: ${content}`)
+let callTuling = function(message) {
+    if(message.self()){
+        return;
     }
 
-    if(m.self()){
-        return
-    }
+    const contact = message.from();
+    const content = message.content();
+    const room = message.room();
 
-    if(/hello/.test(content)){
-        m.say("hello how are you")
-    }
+    console.log(`Message, From: ${contact} Content: ${message} Room:${room}`);
 
-    if(/room/.test(content)){
-        let keyroom = await Room.find({topic: "test"})
-        if(keyroom){
-            await keyroom.add(contact)
-            await keyroom.say("welcome!", contact)
+    let post_data = querystring.stringify({
+      key: tulingAPIKey,
+      info: content,
+      userid: contact
+    });
+
+    let options = {
+      host: 'www.tuling123.com',
+      port: 80,
+      path: '/openapi/api',
+      method: 'POST',
+      rejectUnauthorized: false,
+      headers: {
+        "Content-Type": 'application/x-www-form-urlencoded', //这个一定要有
+      }
+    };
+
+    let req = http.request(options, function(res) {
+        console.log('Response');
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+
+        if(res.statusCode === 200){
+            res.on('data', function(chunk) {
+                console.log('BODY: ' + chunk);
+                tulingCallBack(message,chunk);
+            });
+        }else{
+            tulingCallBack(message,{"code":100000,"text":"今天心情不太好，不理你了！"});
         }
-    }
+    });
 
-    if(/out/.test(content)){
-        let keyroom = await Room.find({topic: "test"})
-        if(keyroom){
-            await keyroom.say("Remove from the room", contact)
-            await keyroom.del(contact)
-        }
-    }
-})
+    req.on('error', function (e) {  
+        console.log('problem with request: ' + e.message);  
+    });  
 
-.init()
+    req.write(post_data);
+    req.end();
+}
+
+let tulingCallBack = function(message,chunk){
+    let data = JSON.parse(chunk);
+    if(/hello/.test(message.content())){
+        //TODO   
+    }
+    message.say(data["text"]);
+}
